2c91b70f30e935c50561a91b011a097f68f9cc70
Joshua de Lange <joshua.delange@gmail.com>
2013-08-23 17:51:16 +0200

    finalizes location add form

--- app/scripts/models/location_model.js
+++ app/scripts/models/location_model.js
@@ -3,5 +3,9 @@ DataCollectionApp.Location = DS.Model.extend({
   desc: DS.attr('string'),
   lat: DS.attr('number'),
   lon: DS.attr('number'),
+  email: DS.attr('string'),
+  phone: DS.attr('string'),
+  website: DS.attr('string'),
+  security_level: DS.attr('number'),
   syncedWithServer: DS.attr('boolean', { defaultValue: false })
 }) ;
