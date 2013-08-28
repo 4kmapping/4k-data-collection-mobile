9e59a0174f18e02d3a538e9c345e4f73f813146d
Joshua de Lange <joshua.delange@gmail.com>
2013-08-28 23:03:45 +0200

    changes settings links, adds call to action arrows to list items

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -5,10 +5,8 @@ DataCollectionApp.Router.map(function () {
   this.resource('location.add', { path: '/add' });  
 
   this.resource('settings', { path: '/settings' }) ;
-  this.resource('settings.new_validation_code', { path: '/settings/new_validation_code' }) ;
-  this.resource('settings.privacy', { path: '/settings/privacy' }) ;
+  this.resource('settings.app_code', { path: '/settings/new_validation_code' }) ;
   this.resource('settings.about', { path: '/settings/about' }) ;
   this.resource('settings.feedback', { path: '/settings/feedback' }) ;
-  this.resource('settings.logout', { path: '/settings/logout' }) ;
 
 });
