4ee61fceb5b4d3ca5ddce56bf0b0e04b1ba4b961
Joshua de Lange <joshua.delange@gmail.com>
2013-09-10 12:59:36 +0200

    sets map to current location by default

--- ember/app/scripts/views/location_map_view.js
+++ ember/app/scripts/views/location_map_view.js
@@ -7,24 +7,38 @@ DataCollectionApp.LocationMapView = Ember.CollectionView.extend({
   didInsertElement: function() {
 
     var mapOptions = {
-        disableDefaultUI: true,
-        zoom: 14,
-        center: new google.maps.LatLng(52.370216, 4.895168),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+      //UI's are for desktops, ieuw.
+      disableDefaultUI: true,
+      zoom: 14,
+      //set to amsterdam by default.
+      //TODO: less biased default location? :)
+      center: new google.maps.LatLng(52.370216, 4.895168),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+      },
+      given = this ;
 
+    //set the map as a controller property
     this.set('map', new google.maps.Map(this.$()[0], mapOptions));
 
+    //set to current location
+    navigator.geolocation.getCurrentPosition(function(pos){
+      given.get('map').setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+    }) ;
+
+    //continue to render them markers!
     this.renderMarkers() ;
 
   },
 
   renderMarkers: function(){
 
+    //cb context!
     var given = this ;
 
+    //for every location
     this.get('controller.content').forEach(function(location){
 
+      // create a new marker
       new google.maps.Marker({
           position: new google.maps.LatLng(location.get('lat'), location.get('lon')),
           map: given.get('map'),
@@ -33,6 +47,6 @@ DataCollectionApp.LocationMapView = Ember.CollectionView.extend({
 
     });
 
-  }.observes('controller.content.@each')
+  }.observes('controller.content.@each') //and call this function everytime the locations change
 
 });
