7ce900e1b53693780e860428a7663f1d95d5bac3
Joshua de Lange <joshua.delange@gmail.com>
2013-09-10 15:43:38 +0200

    adds pretty animation for offline mode toggle

--- ember/app/scripts/controllers/application_controller.js
+++ ember/app/scripts/controllers/application_controller.js
@@ -1,5 +1,8 @@
 DataCollectionApp.ApplicationController = Ember.Controller.extend({
+
   default_security_setting: 2, //'only my group'
+
+  offlineMode: true,
 
   //by default, hide them settings
   showingSettings: false,
