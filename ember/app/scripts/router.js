7912f9766cae3ac05e57a5f06a7b7605d5b8ea1a
Joshua de Lange <joshua.delange@gmail.com>
2013-09-10 10:26:42 +0200

    fixes initial load bug

--- ember/app/scripts/router.js
+++ ember/app/scripts/router.js
@@ -1,9 +1,10 @@
 DataCollectionApp.Router.map(function () {
 
-  this.resource('locations', function(){
+  this.resource('locations', { path: '/' }, function(){
     this.resource('location.map', { path: '/' });
     this.resource('location.list', { path: '/list' });
   }) ;
+
   this.resource('location.add', { path: '/add' });  
 
   this.resource('settings', { path: '/settings' }) ;
