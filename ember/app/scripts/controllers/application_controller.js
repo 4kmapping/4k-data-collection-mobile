8fd61c70977a853e91133b1911c051b1c9e80ed3
Joshua de Lange <joshua.delange@gmail.com>
2013-09-12 12:37:31 +0200

    automatically toggles offline mode

--- ember/app/scripts/controllers/application_controller.js
+++ ember/app/scripts/controllers/application_controller.js
@@ -3,7 +3,12 @@ DataCollectionApp.ApplicationController = Ember.Controller.extend({
   //TODO: move this into settings table
   default_security_setting: 2, //'only my group'
 
-  offlineMode: false,
+  //window offline/online bindings in app router
+  offlineMode: !navigator.onLine,
+  //used to switch things around
+  processOfflineMode: function(){
+    console.log('do stuff') ;
+  }.observes('offlineMode'),
 
   //by default, hide them settings
   showingSettings: false,
--- ember/app/scripts/routes/application_route.js
+++ ember/app/scripts/routes/application_route.js
@@ -1,1 +1,15 @@
-DataCollectionApp.ApplicationRoute = Ember.Route.extend();
+DataCollectionApp.ApplicationRoute = Ember.Route.extend({
+
+  setupController: function(controller){
+
+    $(window).on('offline', function(){
+      controller.set('offlineMode', true) ;
+    }) ;
+
+    $(window).on('online', function(){
+      controller.set('offlineMode', false) ;
+    }) ;
+
+  }
+
+});
